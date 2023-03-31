fd641ef9c386794634efabfef44b6aae4432cc36
Yirui <yirui091@student.ubc.ca>
2023-03-30 21:44:02 -0700

    Before ddl but not finished

--- src/main/ui/ExpenseGUI.java
+++ src/main/ui/ExpenseGUI.java
@@ -1,49 +1,55 @@
 package ui;
 
-import java.awt.Color;
-import java.awt.Font;
+import model.Expense;
+import model.ListOfExpenses;
+
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
-import javax.swing.JTextField;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.net.URL;
+import javax.imageio.ImageIO;
+import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.text.JTextComponent;
+import javax.swing.text.html.HTMLEditorKit;
 
 public class ExpenseGUI {
     @SuppressWarnings("methodlength")
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        // create JFrame and JTable
+        // EFFECTS: create JFrame and JTable
         JFrame frame = new JFrame();
         JTable table = new JTable();
+        Expense expense = new Expense("Saveon", 3.3, "cad", 10, "bmo");
+        ListOfExpenses expenses = new ListOfExpenses();
 
-        // create a table model and set a Column Identifiers to this model
+        // EFFECTS: create a table model and set a Column Identifiers to this model
         Object[] columns = {"Description","Date","Currency","Amount", "Account"};
         DefaultTableModel model = new DefaultTableModel();
         model.setColumnIdentifiers(columns);
 
-        // set the model to the table
+        // EFFECTS: set the model to the table
         table.setModel(model);
 
-        // Change A JTable Background Color, Font Size, Font Color, Row Height
+        // EFFECTS: Change A JTable Background Color, Font Size, Font Color, Row Height
         table.setBackground(Color.LIGHT_GRAY);
         table.setForeground(Color.black);
         Font font = new Font("",1,22);
         table.setFont(font);
         table.setRowHeight(30);
 
-        // create JTextFields
+        // EFFECTS: create JTextFields
         JTextField textDescription = new JTextField();
         JTextField textDate = new JTextField();
         JTextField textCurrency = new JTextField();
         JTextField textAmount = new JTextField();
         JTextField textAccount = new JTextField();
 
-        // create JButtons
+        // EFFECTS: create JButtons
         JButton btnAdd = new JButton("Add");
         JButton btnDelete = new JButton("Delete");
         JButton btnUpdate = new JButton("Update");
@@ -55,33 +61,33 @@ public class ExpenseGUI {
         textAccount.setBounds(20, 340, 100, 25);
 
         btnAdd.setBounds(150, 220, 100, 25);
-        btnUpdate.setBounds(150, 265, 100, 25);
-        btnDelete.setBounds(150, 310, 100, 25);
+        btnUpdate.setBounds(150, 250, 100, 25);
+        btnDelete.setBounds(150, 280, 100, 25);
 
-        // create JScrollPane
+        // EFFECTS: create JScrollPane
         JScrollPane pane = new JScrollPane(table);
-        pane.setBounds(0, 0, 880, 200);
+        pane.setBounds(0, 0, 500, 200);
 
         frame.setLayout(null);
 
         frame.add(pane);
 
-        // add JTextFields to the jframe
+        // EFFECTS: add JTextFields to the jframe
         frame.add(textDescription);
         frame.add(textDate);
         frame.add(textCurrency);
         frame.add(textAmount);
         frame.add(textAccount);
 
-        // add JButtons to the jframe
+        // EFFECTS: add JButtons to the jframe
         frame.add(btnAdd);
         frame.add(btnDelete);
         frame.add(btnUpdate);
 
-        // create an array of objects to set the row data
+        // EFFECTS: create an array of objects to set the row data
         Object[] row = new Object[5];
 
-        // button add row
+        // EFFECTS: button add row
         btnAdd.addActionListener(new ActionListener() {
 
             @Override
@@ -95,10 +101,17 @@ public class ExpenseGUI {
 
                 // add row to the model
                 model.addRow(row);
+                expense.updateDescription(textDescription.getText());
+                expense.updateDescription(textDate.getText());
+                expense.updateDescription(textCurrency.getText());
+                expense.updateDescription(textAmount.getText());
+                expense.updateDescription(textAccount.getText());
+
+                expenses.addExpense(expense);
             }
         });
 
-        // button remove row
+        // EFFECTS: button remove row
         btnDelete.addActionListener(new ActionListener() {
 
             @Override
@@ -109,13 +122,14 @@ public class ExpenseGUI {
                 if (i >= 0) {
                     // remove a row from jtable
                     model.removeRow(i);
+                    expenses.removeExpense(i);
                 } else {
                     System.out.println("Delete Error");
                 }
             }
         });
 
-        // get selected row data From table to textfields
+        // EFFECTS: Get selected row data From table to textfields
         table.addMouseListener(new MouseAdapter() {
 
             @Override
@@ -132,7 +146,7 @@ public class ExpenseGUI {
             }
         });
 
-        // button update row
+        // EFFECTS: button update row
         btnUpdate.addActionListener(new ActionListener() {
 
             @Override
@@ -147,16 +161,99 @@ public class ExpenseGUI {
                     model.setValueAt(textCurrency.getText(), i, 2);
                     model.setValueAt(textAmount.getText(), i, 3);
                     model.setValueAt(textAccount.getText(),i,4);
+
+                    expense.updateDescription(textDescription.getText());
+                    expense.updateDescription(textDate.getText());
+                    expense.updateDescription(textCurrency.getText());
+                    expense.updateDescription(textAmount.getText());
+                    expense.updateDescription(textAccount.getText());
                 } else {
                     System.out.println("Update Error");
                 }
             }
         });
 
-        frame.setSize(900,400);
+        frame.setSize(900,1440);
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
 
+        final String filename = "ExpensesGUI.java";
+        Container content = frame.getContentPane();
+
+        final JEditorPane editorPane = new JEditorPane();
+        JScrollPane scrollPane = new JScrollPane(editorPane);
+        content.add(scrollPane, BorderLayout.CENTER);
+
+        editorPane.setEditorKit(new HTMLEditorKit());
+
+        JPanel panel = new JPanel();
+
+        // EFFECTS: Load data
+        Action loadAction = new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    doLoadCommand(editorPane, filename);
+                } catch (Exception e1) {
+                    e1.printStackTrace();
+                }
+            }
+        };
+
+        loadAction.putValue(Action.NAME, "Load");
+        JButton btnLoad = new JButton(loadAction);
+        //panel.add(btnLoad);
+        btnLoad.setBounds(150,340,100,25);
+        frame.add(btnLoad);
+
+        // EFFECTS: Save data
+        Action saveAction = new AbstractAction() {
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    doSaveCommand(editorPane, filename);
+                } catch (Exception e1) {
+                    e1.printStackTrace();
+                }
+            }
+        };
+
+        saveAction.putValue(Action.NAME, "Save");
+        JButton btnSave = new JButton(saveAction);
+        //panel.add(btnSave);
+        btnSave.setBounds(150,310,100,25);
+        frame.add(btnSave);
+
+         //EFFECTS: Display an image
+        BufferedImage img = ImageIO.read(new URL(
+                "https://encrypted-tbn0.gstatic.com/images?q="
+                        +
+                        "tbn:ANd9GcSZUmIp0fj4x88KXfc1KJ-H4Xx_tKYun_HEQA&usqp=CAU"));
+        ImageIcon icon = new ImageIcon(img);
+        frame.setLayout(new BoxLayout(content, BoxLayout.Y_AXIS));
+        JLabel lbl = new JLabel(icon);
+        lbl.setLocation(100,50);
+        frame.add(lbl);
+        lbl.setIcon(icon);
+        lbl.setLocation(100,50);
+        content.add(lbl);
+
+        content.add(panel, BorderLayout.SOUTH);
+
+        frame.setVisible(true);
     }
+
+    public static void doSaveCommand(JTextComponent textComponent, String filename) throws Exception {
+        FileWriter writer = null;
+        writer = new FileWriter(filename);
+        textComponent.write(writer);
+        writer.close();
+    }
+
+    public static void doLoadCommand(JTextComponent textComponent, String filename) throws Exception {
+        FileReader reader = null;
+        reader = new FileReader(filename);
+        textComponent.read(reader, filename);
+        reader.close();
+    }
+
 }
---
+++ src/main/ui/MainGUI.java
@@ -0,0 +1,11 @@
+//package ui;
+//
+//import javax.swing.*;
+//import java.io.IOException;
+//
+//public class MainGUI {
+//    public void main(String[] args) throws IOException {
+//        JFrame frame = new JFrame();
+//        frame.add(new ExpenseGUI());
+//    }
+//}
